51bda222a7738a4fe4ff8fdb7996800c911abd76
s19205 <61733302+s19205@users.noreply.github.com>
2020-03-14 13:48:20 +0100

    01

--- Cw2/Cw2/Program.cs
+++ Cw2/Cw2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
@@ -10,7 +11,7 @@ namespace Cw2
 
         public static void ErrorLogging(Exception ex)
         {
-            string logpath = @"Z:\4 semestr\APBD\cw2\log.txt";
+            string logpath = @"Z:\4semestr\APBD\cw2\log.txt";
             if (!File.Exists(logpath))
             {
                 File.Create(logpath).Dispose();
@@ -23,39 +24,79 @@ namespace Cw2
             sw.Close();
         }
 
+        public static Dictionary<string, int> studies(string[] source)
+        {
+            var map = new Dictionary<string, int>();
+            
+            foreach (var str in source)
+            {
+                var fields = str.Split(',');
+
+                string key = fields[2];
+                if (!map.ContainsKey(key))
+                {
+                    map.Add(key, 1);
+                }
+                else
+                {
+                    map[key] = map[key] + 1;
+                }
+
+            }
+            return map;
+        }
+
         static void Main(string[] args)
         {
             try
             {
-                string csvpath = Console.ReadLine();    //Z:\4 semestr\APBD\cw2\dane.csv
-                string xmlpath = Console.ReadLine();    //Z:\4 semestr\APBD\cw2\
+                string csvpath = Console.ReadLine();    //Z:\4semestr\APBD\cw2\dane.csv
+                string xmlpath = Console.ReadLine();    //Z:\4semestr\APBD\cw2\
                 string format = Console.ReadLine();     //xml
+
+                var map = new Dictionary<string, int>();
+                
                 if (File.Exists(csvpath) && Directory.Exists(xmlpath))
                 {
                     // Read into an array of strings.  
+                    var dateTimeNow = DateTime.Now; 
+                    var dateOnlyString = dateTimeNow.ToShortDateString();
                     string[] source = File.ReadAllLines(csvpath);
-                    XElement xml = new XElement("uczelnia",
-                        new XElement("studenci",
-                        from str in source
-                        let fields = str.Split(',')
-                        select new XElement("student",
-                                new XAttribute("indexNumber", "s" + fields[4]),
-                                new XElement("fname", fields[0]),
-                                new XElement("lname", fields[1]),
-                                new XElement("birthday", fields[5]),
-                                new XElement("email", fields[6]),
-                                new XElement("mothersname", fields[7]),
-                                new XElement("fathersname", fields[8]),
-                                new XElement("studies",
-                                    new XElement("name", fields[2]),
-                                    new XElement("mode", fields[3])
+
+                    map = studies(source);
+
+                    XElement xml = new XElement("uczelnia", 
+                        new XAttribute("createdAt", DateTime.Now.ToString("dd.MM.yyyy")),
+                        new XAttribute("autor","Polina Lozovska"),
+                            new XElement("studenci",
+                            from str in source
+                            let fields = str.Split(',')
+                                select new XElement("student",
+                                    new XAttribute("indexNumber", "s" + fields[4]),
+                                    new XElement("fname", fields[0]),
+                                    new XElement("lname", fields[1]),
+                                    new XElement("birthday", fields[5]),
+                                    new XElement("email", fields[6]),
+                                    new XElement("mothersname", fields[7]),
+                                    new XElement("fathersname", fields[8]),
+                                    new XElement("studies",
+                                        new XElement("name", fields[2]),
+                                        new XElement("mode", fields[3])
+                                    )
+                                )
+                            ),
+                            new XElement("activeStudies",
+                                from item in map
+                                select new XElement("studies", 
+                                    new XAttribute("name", item.Key),
+                                    new XAttribute("numberOfStudents", item.Value)
                                 )
                             )
-                        )
                     );
                     xml.Save(String.Concat(xmlpath + "result.xml"));
-
-                } else{
+                }
+                else
+                {
                     if (!File.Exists(csvpath))
                     {
                         throw new Exception("File does not exist");
